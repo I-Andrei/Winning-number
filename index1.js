a995dbc4c0f47c63bac67c2cec52fa4881c013b1
I-Andrei <78320950+I-Andrei@users.noreply.github.com>
2022-06-23 17:25:05 +0300

    Changing to let and correcting

--- index1.js
+++ index1.js
@@ -1,15 +1,15 @@
-var winnerNumber;
+let winnerNumber;
 
 function createButtons(){
-   var x = ""
-   for(i = 1; i <= document.getElementById("numberOfButtons").value; ++i)
+   let x = ""
+   for (let i = 1; i <= document.getElementById("numberOfButtons").value; ++i)
       x += "<button type=button onclick=winnerButton(" + i + ");> Button" + i + "</button>"
    document.getElementById('MyID').innerHTML = x
    winnerNumber = Math.floor(Math.random() * ((document.getElementById("numberOfButtons").value) - 1) + 1);
  }
  
  function winnerButton(s){
-  if(s==winnerNumber)
+  if (s==winnerNumber)
     document.getElementById('MyID').innerHTML = winnerNumber
   else
     document.getElementById('MyID').innerHTML = winnerNumber
